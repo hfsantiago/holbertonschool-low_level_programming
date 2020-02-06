7dad3855feddb8337bf968aad928616895039538
hfsantiago <santiquiu@gmail.com>
2020-02-06 20:22:48 +0000

    Add 3-print_alphabets.c file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - entry block
+*Return: Always 0 (Success)
+**/
+int main(void)
+{
+char c, d;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+for (d = 'A'; d <= 'Z'; d++)
+putchar(d);
+putchar('\n');
+return (0);
+}
