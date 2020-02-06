be42beb42957cf97a69dba55947a7d51f72621c2
hfsantiago <santiquiu@gmail.com>
2020-02-06 20:52:09 +0000

    Add 5-print_numbers.c solutions

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
 /**
 * main - Entry point
-* @void: no argument
 * Return: Always 0 (Success)
 **/
 int main(void)
 {
-	int i;
+int i;
 
-	for (i = 0; i < 10; i++)
-		printf("%d", i);
-	printf("\n");
-	return (0);
+for (i = 0; i < 10; i++)
+printf("%d", i);
+printf("\n");
+return (0);
 }
