1b5d2a131865766d9b5df36c6bea11bbe7b66374
hfsantiago <santiquiu@gmail.com>
2020-02-06 22:00:46 +0000

    Add 8-print_base16.c solutions

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - entry point
+*Return: 0
+*/
+int main(void)
+{
+int i;
+char c;
+for (i = 0; i < 10; i++)
+putchar(i % 10 + '0');
+for (c = 'a'; c < 'g'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
