59244ec67958c58bc80435aa2fd721f24f97d143
hfsantiago <santiquiu@gmail.com>
2020-02-11 18:46:25 +0000

    Add 0-holberton.c solution 3.0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,26 +1,21 @@
 #include "holberton.h"
-#include <string.h>
 
 /**
  * main - Entry point
  *
- * Return:  Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char *text;
-	int len;
-
-	text = "Holberton";
-	len = strlen(text);
-
-	for (int i = 0; i <= len; i++)
-	{
-		_putchar(text[i]);
-	}
-
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
-
 	return (0);
 }
-
