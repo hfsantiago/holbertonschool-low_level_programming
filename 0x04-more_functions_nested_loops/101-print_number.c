1bbd6196ee18912b0f6038fe53961a9f138b3782
hfsantiago <santiquiu@gmail.com>
2020-02-14 05:42:58 +0000

    Add 101-print_number.c solutions 2.0

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,37 +1,42 @@
 #include "holberton.h"
-#include <stdio.h>
-
 /**
- * print_number - Print a integer
- * @n: Number
- */
+  * print_number - print numbers out
+  * @n: takes in an integer
+  */
 void print_number(int n)
 {
-	int  digs[10], i;
+	int i, j, div;
+	int x = n;
 
-	if (n == 0)
+	div = 10;
+
+	if (n < 0)
 	{
-		_putchar('0');
-		return;
-	}
-	else if (n < 0)
-	{
-		n = -n;
+		x = x * -1;
 		_putchar('-');
 	}
-
-	i = 0;
-	while (n > 0)
+	else if (n == 0)
 	{
-		digs[i] = n % 10;
-		n = n / 10;
-		i++;
+		_putchar('0');
 	}
-
-	i = i - 1;
-
-	for (; i >= 0; i--)
+	else
 	{
-		_putchar(digs[i] + 48);
+		for (i = 1; x > 0; i++)
+		{
+			x /= 10;
+		}
+			for (j = 0; j < i - 1; j++)
+			{
+				div *= 10;
+
+
+			while (div >= 1)
+			{
+				_putchar(n / div + '0');
+				n = n % div;
+				div = div / 10;
+
+			}
+		}
 	}
 }
