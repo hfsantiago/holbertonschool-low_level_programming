c0244d9f3a054f84d8e02ee6781c4d9dbecb1acb
hfsantiago <santiquiu@gmail.com>
2020-02-18 20:29:10 +0000

    Add 8-print_array.c solutions

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+i#include <stdio.h>
+/**
+  * print_array - prints n number of elements in array
+  * @a: array of type int and pointer
+  * @n: number of elements to print
+  */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != (n - 1))
+			printf(", ");
+	}
+	printf("\n");
+}
