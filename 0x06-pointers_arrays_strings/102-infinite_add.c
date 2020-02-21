bdc9eb7e95e57c2bbbe6d9b2f219270795e598e5
hfsantiago <santiquiu@gmail.com>
2020-02-21 05:12:42 +0000

    Add 102-infinite_add.c solution 1.3

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -1,77 +1,72 @@
 #include "holberton.h"
-#include <stdio.h>
-int _strlen(char *s);
 
 /**
-  *infinite_add - add two numbers that are chars
-  *@n1: first number
-  *@n2: second number
-  *@r: result buffer
-  *@size_r: size of result buffer
-  *Return: number with difference
-  */
+ * infinite_add - adds two numbers
+ * @n1: first number to add
+ * @n2: second number to add
+ * @r: buffer for result
+ * @size_r: size of r
+ *
+ * Return: on success, pointer to r, otherwise 0
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, max,  max_n1, max_n2;
-	int carry = 0, value = 0;
-	char aux_arr1[100];
-	char aux_arr2[50];
+	int i, add;
+	int max = size_r;
+	int c1 = 0, c2 = 0;
+	int dif1 = 0, dif2 = 0;
 
-	max_n1 = _strlen(n1);
-	max_n2 = _strlen(n2);
-	if (max_n1 > max_n2)
+	while (n1[c1] != '\0')
+		c1++;
+
+	while (n2[c2] != '\0')
+		c2++;
+
+/*	if (c1 > size_r - 1 || c2 > size_r - 1)
+		return (0);
+*/
+	if (c1 > c2)
 	{
-		max = max_n1;
-		for (i = max; i > 0; i++)
-		{
-			aux_arr1[max] = n1[i];
-			if (i >= (max - max_n2))
-				aux_arr2[i] = n2[i - max - max_n2];
-			else
-				aux_arr2[i] = '0';
-		}
+		i = c1;
+		dif1 = c1 - c2;
+	}
+	else if (c2 > c1)
+	{
+		i = c2;
+		dif2 = c2 - c1;
 	}
 	else
 	{
-		max = max_n2;
-		for (i = 0; i < max; i++)
+		i = c1;
+	}
+
+	for (i = i - 1; i >= 0; i--, max--)
+	{
+		if (i - dif1 < 0)
 		{
-			aux_arr1[i] = n2[i];
-			if (i >= (max - max_n1))
-				aux_arr2[i] = n1[i - max - max_n1];
-			else
-				aux_arr2[i] = '0';
+			r[i] = n1[i];
+		}
+		else if (i - dif2 < 0)
+		{
+			r[i] = n2[i];
+		}
+		else
+		{
+			add += (n1[i - dif2] - '0') + (n2[i - dif1] - '0');
+			r[i] = add % 10;
+		}
+
+		if (add > 9)
+		{
+			if (max == 0)
+				return (0);
+			add = 1;
+		}
+		else
+		{
+			add = 0;
 		}
 	}
-	printf("valor maximo: %d \n", max);
-	printf("auxiliar 1: %s \n", aux_arr1);
-	printf("auxiliar 2: %s \n", aux_arr2);
-	printf("original 1: %s \n", n1);
-	printf("original 2: %s \n", n2);
-	for (i = 0; i < size_r; i++)
-	{
-		value = (aux_arr1[max - 1 - i] - '0') + (aux_arr2[max - 1 - i] - '0');
-		value = value + carry;
-		r[size_r - 1 - i] = value % 10 + '0';
-		carry = value / 10;
-	}
+
 	return (r);
-}
-
-/**
- *_strlen - returns data len
- *@s: input string
- *
- *Return:an int number
- *
- */
-int _strlen(char *s)
-{
-	int counter = 0;
-
-	while (*(s + counter) != 0)
-	{
-	counter++;
-	}
-	return (counter);
 }
