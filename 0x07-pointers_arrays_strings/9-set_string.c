90b44b28c26be1bb2879d81121b459666cc2c959
hfsantiago <santiquiu@gmail.com>
2020-02-25 02:48:00 +0000

    Add 9-set_string.c solutions

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * set_string - Set the value of a pointer to a char
+ * @s: Pointer of a pointer
+ * @to: Pointer to
+ *
+ *
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
