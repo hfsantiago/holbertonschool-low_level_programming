89e8fe7acdc67ebecb14afeb57cf61892fc06914
hfsantiago <santiquiu@gmail.com>
2020-02-26 21:27:59 +0000

    Add 0-puts_recursion.c solutions 1.0

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,16 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+void _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int helper_julien(int x, int y);
+int helper_prime(int x, int y);
+int _check(char *s, int l);
+int wildcmp(char *s1, char *s2);
+#endif
