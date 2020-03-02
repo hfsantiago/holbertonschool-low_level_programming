fe901407ae093e5f6eff84c53c9e32712f83b87c
hfsantiago <santiquiu@gmail.com>
2020-03-02 01:20:12 +0000

    Add 0-whatsmyname.c solutions 3.0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,10 @@
 #include<stdio.h>
+/**
+ * main - Entry function
+ * @argc: Size of the argv
+ * @argv: Array of arguments
+ * Return: Return 0
+ **/
 int main(int __attribute__((unused)) argc, char **argv)
 {
 	printf("%s\n", argv[0]);
