a25f03a6aa7c517eaf0c3607488fd29ff32bc556
hfsantiago <santiquiu@gmail.com>
2020-03-02 01:13:46 +0000

    Adds fixed file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
-int main (int __attribute__((unused)) argc , char **argv)
+int main(int __attribute__((unused)) argc, char **argv)
 {
-	printf("%s\n" , argv[0]);
+	printf("%s\n", argv[0]);
 
-	return(0);
+	return (0);
 }
 
