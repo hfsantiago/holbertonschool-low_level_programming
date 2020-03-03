be9cf79cabcd7c8c1bdd5c781d5f7a5bb1674445
emmanueles1993 <emmanueles1993@hotmail.com>
2020-03-03 16:17:32 -0500

    Add 5-argstostr.c fixed 1.0

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: number of command line arguments
+ * @av: passed arguments
+ * Return: new string
+ */
+char *argstostr(int ac, char **av)
+{
+	char *newstring;
+
+	int i, j, k, len;
+
+	len = 0;
+	k = 0;
+
+	if (ac < 1 || av == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < ac; i++)
+			for (j = 0; av[i][j]; j++)
+			{
+				len++;
+			}
+		newstring = malloc((len + 1 + ac) * sizeof(char));
+		if (newstring == NULL)
+		{
+			return (NULL);
+		}
+		for (i = 0; i < ac; i++)
+		{
+			for (j = 0; av[i][j]; j++)
+			{
+				newstring[k++] = av[i][j];
+			}
+			newstring[k++] = '\n';
+		}
+		newstring[k] = '\0';
+	}
+	return (newstring);
+}
