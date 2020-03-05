42de95c6d0b1dc31f2718e91c5f9b5739b279e53
hfsantiago <santiquiu@gmail.com>
2020-03-05 16:51:32 -0500

    Add 1-string_nconcat.c solutions 2.0

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,40 +1,46 @@
 #include "holberton.h"
 /**
- * string_nconcat - Concatenates two strings.
- * @s1: First String.
- * @s2: Second String.
- * @n: Bytes to show in screen.
- *
- * Return: Return both strings concatenated.
+ * string_nconcat - concatenate 2 strings, only n bytes of s2
+ * @s1: string 1
+ * @s2: string 2
+ * @n: bytes to include of s2
+ * Return: NULL if fail, else pointer to malloc memory
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j;
-	char *str;
+	unsigned int i, j, k, l;
+	char *ptr;
 
-	(s1 == NULL ? s1 = "" : 0);
-	(s2 == NULL ? s2 = "" : 0);
+	l = 0;
+	if (s1 == 0)
+		s1 = "";
 
-	for (i = 0; *(s1 + i) != '\0'; i++)
+	if (s2 == 0)
+		s2 = "";
+
+	for (i = 0 ; s1[i] != 0 ; i++)
 		;
 
-	for (j = 0; *(s2 + j) != '\0'; j++)
+	for (j = 0 ; s2[j] != 0 ; j++)
 		;
 
-	(n >= j ? n = j : n);
+	if (n >= j)
+	{
+		n = j;
+		ptr = malloc(i + n + 1);
+	}
+	else
+		ptr = malloc(i + n + 1);
 
-	str = malloc(1 + (i + n) * sizeof(*str));
-
-	if (str == NULL)
+	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; *(s1 + i) != '\0'; i++)
-		*(str + i) = *(s1 + i);
+	for (k = 0 ; k < i ; k++)
+		ptr[k] = s1[k];
 
-	for (j = 0; j < n; j++, i++)
-		*(str + i) = *(s2 + j);
+	for (; k < (i + n) ; k++, l++)
+		ptr[k] = s2[l];
 
-	*(str + i) = '\0';
-
-	return (str);
+	ptr[i + n] = 0;
+	return (ptr);
 }
