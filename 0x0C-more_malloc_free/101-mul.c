096af4c708e4ea9e6e547b563e2ae4d26e5b5c86
hfsantiago <santiquiu@gmail.com>
2020-03-05 21:45:25 -0500

    Add 101-mul.c 2.0

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,108 +1,94 @@
+#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "holberton.h"
-#include <string.h>
-
 /**
- * main - multiple two positive numbers, it takes two arg
- * @argc: argument count
- * @argv: number to multiply
- * prints out num
- * Return: 0
- */
-
-int main(int argc, char *argv[])
-{
-	int length, carry, prod, i, j, len1, len2;
-	int *result;
-
-	if (argc != 3 || !(_isNum(argv[1])) || !(_isNum(argv[2])))
-	{
-		puts("Error");
-		exit(98);
-	}
-	len1 = _strlen(argv[1]), len2 = _strlen(argv[2]);
-	length = len1 + len2;
-	result = calloc(length, sizeof(int *));
-	if (result == NULL)
-		puts("Error"), exit(98);
-	for (i = len2 - 1; i > -1; i--)
-	{
-		carry = 0;
-		for (j = len1 - 1; j > -1; j--)
-		{
-			prod = (argv[2][i] - '0') * (argv[1][j] - '0');
-			carry =  (prod / 10);
-			result[(i + j) + 1] += (prod % 10);
-			if (result[(i + j) + 1] > 9)
-			{
-				result[i + j] += result[(i + j) + 1] / 10;
-				result[(i + j) + 1] = result[(i + j) + 1] % 10;
-			}
-			result[(i + j)] += carry;
-		}
-	}
-	if (result[0] == 0)
-		i = 1;
-	else
-		i = 0;
-	for (; i < length; i++)
-		printf("%d", result[i]);
-	printf("\n");
-	free(result);
-	return (0);
-}
-
-/**
- * _isNum - checks if it is fully a number
- * @num: string to check
- * Return: 1 if all num 0 if not
- */
-
-int _isNum(char *num)
-{
-	int i;
-
-	for (i = 0; num[i] != '\0'; i++)
-	{
-		if (num[i] < '0' || num[i] > '9')
-			return (0);
-	}
-	return (1);
-}
-
-/**
- *_memset - sets first n bytes of the memory area
- * @s: array to set
- * @b: what to set it to
- * @n: first n bytes
- * Return: s
- */
-
-void *_memset(void *s, int b, unsigned int n)
-{
-	if (n)
-	{
-		char *d = s;
-
-		do {
-			*d++ = b;
-		} while (--n);
-	}
-	return (s);
-}
-
-/**
- * _strlen - size of string
- * @s: string to measure
- * Return: size of string
+ * _strlen - measure string length.
+ * @s: value to size.
+ * Return: string length.
  */
 int _strlen(char *s)
 {
+	int count;
+
+	for (count = 0; s[count] != 0; count++)
+		;
+	return (count);
+}
+/**
+ * _isdigit - check for digits
+ * Description: return 1 or 0 if character
+ * is digit or not
+ * @c: Digit to check
+ * Return: On success 1, 0 on reject.
+ */
+int _isdigit(char *c)
+{
 	int i;
 
-	i = 0;
-	while (*(s + i) != '\0')
-		++i;
-	return (i);
+	for (i = 0; c[i]; i++)
+	{
+		if (!(c[i] >= '0' && c[i] <= '9'))
+			return (1);
+	}
+	return (0);
+}
+/**
+ * check_zero - Check if array has plenty zeros.
+ * @zeros: Array to check
+ * Return: 1 or 0.
+ */
+int check_zero(char *zeros)
+{
+	if (zeros[0] == 0 && zeros[1] == 0 && zeros[1] == 0)
+		return (1);
+	return (0);
+}
+/**
+ * main - Multiplies two numbers.
+ * @ac: Arguments number
+ * @av: Array containing arguments
+ * Return: Multiplication.
+ */
+int main(int ac, char **av)
+{
+	int e, i, j, carry, total_size, size_n1, size_n2, res_m;
+	char err[6] = "Error", *f_res;
+
+	if (ac != 3 || _isdigit(av[1]) == 1 || _isdigit(av[2]) == 1)
+	{
+		for (e = 0; err[e] != '\0'; e++)
+			_putchar(err[e]);
+		_putchar('\n');
+		exit(98);
+	}
+	size_n1 = _strlen(av[1]), size_n2 = _strlen(av[2]);
+	total_size = size_n1 + size_n2;
+	f_res = malloc(sizeof(int *) * (size_n1 + size_n2));
+	if (f_res == NULL)
+		return (1);
+	for (i = size_n2 - 1; i >= 0; i--)
+	{
+		carry = 0;
+		for (j = size_n1 - 1; j >= 0; j--)
+		{
+			res_m = (av[2][i] - '0') * (av[1][j] - '0');
+			carry = (res_m / 10);
+			f_res[(i + j) + 1] += (res_m % 10);
+			if (f_res[(i + j) + 1] > 9)
+			{
+				f_res[i + j] += f_res[(i + j) + 1] / 10;
+				f_res[(i + j) + 1] = f_res[(i + j) + 1] % 10;
+			}
+			f_res[i + j] += carry;
+		}
+	}
+	i = f_res[0] == 0 ? 1 : 0;
+	if (check_zero(f_res) == 1)
+		_putchar('0');
+	else
+		for (; i < total_size; i++)
+			_putchar(f_res[i] + '0');
+	_putchar('\n');
+	free(f_res);
+	return (0);
 }
