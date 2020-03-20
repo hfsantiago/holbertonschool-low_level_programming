56a26b1afbb8f40d736cb6e7d578605e42998b66
hfsantiago <santiquiu@gmail.com>
2020-03-19 22:50:15 -0500

    Add 2-add_node.c file 1.0

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,37 +1,27 @@
 #include "lists.h"
-#include "string.h"
+
 /**
- * add_node - adds a new node at the beginning of a list_t list
- * @head: pointer to a pointer to &head
- * @str: char pointer that holds string
- * Using function strdup to duplicate string
- * Return: address of the new element or NULL if it failed.
+ * add_node - adds a new node at the beginning of a list
+ * @head: pointer to beginning of the list
+ * @str: new string to add to list
+ *
+ * Return: address of the new element, NULL if failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	size_t i;
-	list_t *new;
+	unsigned int count = 0;
+	list_t *new = malloc(sizeof(list_t));
 
-	if (str == NULL)
+	if (new == NULL || str == NULL)
 		return (NULL);
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
+	while (str[count])
+		count++;
 
+	new->str = strdup(str);
+	new->len = count;
 	new->next = *head;
 	*head = new;
-
-	new->str = strdup(str);/*duplicating string*/
-	if (new->str == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-	/*gets length of stirng*/
-	for (i = 0; str[i]; i++)
-		;
-	new->len = i;
 
 	return (new);
 }
