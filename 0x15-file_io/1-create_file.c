d27c04567faa1f427f104eb89c3825e3497192f5
hfsantiago <santiquiu@gmail.com>
2020-03-31 17:24:51 -0500

    Add 1-create_file.c and holberton.h modify 1.0

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,54 @@
+#include "holberton.h"
+/**
+ * _strlen - finds string length
+ * @s: a string
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+	while (s[length] != '\0')
+		length++;
+	return (length);
+}
+/**
+ * create_file - creates a file
+ * @filename: pointer to the file to be created
+ * @text_content: terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int write_to_file;
+	int length;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+
+	length = _strlen(text_content);
+	write_to_file = write(fd, text_content, length);
+	if (write_to_file == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
