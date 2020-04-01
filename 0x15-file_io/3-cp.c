4512a72d522d50fdf0d832c4ebd85a83bd9ff679
hfsantiago <santiquiu@gmail.com>
2020-03-31 21:03:19 -0500

    Add 3-cp.c fixed 1.0

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,66 +1,111 @@
-#include "holberton.h"
-#define size 1204
+#include"holberton.h"
 /**
- * main - entry point and error handling
- * @argc: argument count
- * @argv: arguments after command being executed
- * Return: 0 Success else exit with error code and message
+ * check97 - checks for the correct number of arguments
+ * @argc: number of arguments
+ *
+ * Return: void
  */
-int main(int argc, char **argv)
+void check97(int argc)
 {
-	int errorCheck;
-
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	if (argv[1] && argv[2])
-		errorCheck = cp(argv[1], argv[2]);
-	switch (errorCheck)
+}
+
+/**
+ * check98 - checks that file_from exists and can be read
+ * @check: checks if true of false
+ * @file: file_from name
+ * @fd_from: file descriptor of file_from, or -1
+ * @fd_to: file descriptor of file_to, or -1
+ *
+ * Return: void
+ */
+void check98(ssize_t check, char *file, int fd_from, int fd_to)
+{
+	if (check == -1)
 	{
-	case 98:
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
 		exit(98);
-	case 99:
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	default:
-		return (0);
 	}
-	return (0);
+}
+
+/**
+ * check99 - checks that file_to was created and/or can be written to
+ * @check: checks if true of false
+ * @file: file_to name
+ * @fd_from: file descriptor of file_from, or -1
+ * @fd_to: file descriptor of file_to, or -1
+ *
+ * Return: void
+ */
+void check99(ssize_t check, char *file, int fd_from, int fd_to)
+{
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
+		exit(99);
+	}
+}
+
+/**
+ * check100 - checks that file descriptors were closed properly
+ * @check: checks if true or false
+ * @fd: file descriptor
+ *
+ * Return: void
+ */
+void check100(int check, int fd)
+{
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
 }
 /**
- * cp - function that copies the content of a file to another file
- * @copy: first argument (file name) to copy from
- * @paste: second argument (creates files and copies content)
- * Return: 0- success |98, 99 -read and write fails|exits if cant close fd
+ * main - opies the content of a file to another file.
+ * @argc: number of arguments passed
+ * @argv: array of pointers to the arguments
+ *
+ * Return: 0 on success
  */
-int cp(char *copy, char *paste)
+int main(int argc, char *argv[])
 {
-	int fd, fdC, fdP, count, check, check2;
-	char buffer[size];
+	int fd_from, fd_to, close_to, close_from;
+	ssize_t lenr, lenw;
+	char buffer[1024];
+	mode_t file_perm;
 
-	fdC = open(copy, O_RDONLY);
-	fdP = open(paste, O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	if (fdC == -1 || fdP == -1)
-		return (fdC == -1 ? 98 : 99);
-
-	do {/*read multiple times if more content then given size*/
-	count = read(fdC, buffer, size);
-	if (count == -1)
-		return (98);
-	if (count != 0)
-		check = write(fdP, buffer, count);
-	if (check == -1)
-		return (99);
-	} while (count); /*untill count becomes 0*/
-
-	check = close(fdC), check2 = close(fdP);
-	if (check == -1 || check2 == -1)
+	check97(argc);
+	fd_from = open(argv[1], O_RDONLY);
+	check98((ssize_t)fd_from, argv[1], -1, -1);
+	file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
+	check99((ssize_t)fd_to, argv[2], fd_from, -1);
+	lenr = 1024;
+	while (lenr == 1024)
 	{
-		fd = ((check == -1) ? fdC : fdP);
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
+		lenr = read(fd_from, buffer, 1024);
+		check98(lenr, argv[1], fd_from, fd_to);
+		lenw = write(fd_to, buffer, lenr);
+		if (lenw != lenr)
+			lenw = -1;
+		check99(lenw, argv[2], fd_from, fd_to);
 	}
+	close_to = close(fd_to);
+	close_from = close(fd_from);
+	check100(close_to, fd_to);
+	check100(close_from, fd_from);
 	return (0);
 }
